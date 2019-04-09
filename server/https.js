543e840db2671dd6ac07df572a09ccd5847cf1fd
Bruno Gachie <bga@netinc.fr>
2019-04-10 00:28:19 +0200

    v0.5

--- server/https.js
+++ server/https.js
@@ -1,45 +1,39 @@
-
-
-// Dependencies
+//Dépendances
 require( 'dotenv' )
-const fs = require('fs');
-//const http = require('http');
-const https = require('https');
-const express = require('express');
 const morgan = require( 'morgan' )
+const fs = require( 'fs' )
+const https = require( 'https' )
+const express = require( 'express' )
 
-const app = express();
+console.log( process.env.npm_package_version, process.env.NODE_ENV )
 
-console.log( process.env.NODE_ENV )
+//APPLICATION
+const app = express()
 
-// Certificate
-var credentials
-if ( process.env.NODE_ENV === 'production' ) 
-	credentials = {
-		privateKey: fs.readFileSync('/etc/letsencrypt/live/www.cyber-nation.fr/privkey.pem', 'utf8'),
-		certificate: fs.readFileSync('/etc/letsencrypt/live/www.cyber-nation.fr/cert.pem', 'utf8'),
-		ca: fs.readFileSync('/etc/letsencrypt/live/www.cyber-nation.fr/chain.pem', 'utf8')
-	}
-else 
-	credentials = {
-		key: fs.readFileSync('server/ssl/localhost.key'),
-		cert: fs.readFileSync('server/ssl/localhost.cert')
-	}
-
+//Middlewares
 app.use( morgan( 'dev' ) )
+app.use( express.static( 'public' ) )
 
-app.use((req, res) => {
-	res.send('Hello there !');
-});
+app.use( ( req, res ) => {
+	res.send( 'Hello there !' )
+} )
 
-// Starting both http & https servers
-//const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+//SERVEUR HTTPS
 
-/*httpServer.listen(80, () => {
-	console.log('HTTP Server running on port 80');
-});*/
+//SSL
+var credentials = ( process.env.NODE_ENV === 'production' ) ?
+{
+		key: fs.readFileSync( '/etc/letsencrypt/live/www.cyber-nation.fr/privkey.pem', 'utf8' ),
+	    cert: fs.readFileSync( '/etc/letsencrypt/live/www.cyber-nation.fr/cert.pem', 'utf8' ),
+		ca: fs.readFileSync( '/etc/letsencrypt/live/www.cyber-nation.fr/chain.pem', 'utf8' )
+} : {
+		key: fs.readFileSync('server/ssl/localhost.key'),
+		cert: fs.readFileSync('server/ssl/localhost.crt'),
+		password: process.env.CERT_PASSWORD
+}
 
-httpsServer.listen(443, () => {
-	console.log('HTTPS Server running on port 443');
-});
+//Serveur
+const httpsServer = https.createServer( credentials, app )
+httpsServer.listen( 443, () => {
+	console.log( `Server HTTPS à l'écoute sur le port 443` )
+} )
