92cb447e51ca716fbc77643a59b7ad64d644c73c
Bruno Gachie <bga@netinc.fr>
2019-05-03 19:54:06 +0200

    Candidature ssi connecté

--- public/app/app.js
+++ public/app/app.js
@@ -4,9 +4,10 @@ import '/app/user-name/user-name.js'
 //Candidatures
 import ElectionChoice from '/app/election-choice/election-choice.js'
 
-document.addEventListener( 'registered', () => 
+document.addEventListener( 'registered', () => {
     enroll.href = '#'
-)
+    enroll.removeAttribute( 'disabled' )
+} )
 
 enroll.onclick = () =>
     document.body.appendChild( new ElectionChoice )
